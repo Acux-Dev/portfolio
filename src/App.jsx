9ccfbf8ccabb4e30f6ac1c830066d8069126f6d5
Acux-dev <juansup123@gmail.com>
2024-09-08 22:47:01 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Acux</h1>
+        <p>Portfolio</p>
+        <p>Prueba despliegue en vercel</p>
+      </div>
+    </>
+  )
+}
+
+export default App
